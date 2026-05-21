9db1f8c6d113775ca66ed085b39e4e00e760f7f2
tanzid109 <tanzid.unkhalil109@gmail.com>
2026-05-21 12:38:44 +0600

    database created

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,46 @@
-// import { Pool } from "pg";
-// import config from "../utils/config";
+import { Pool } from "pg";
+import config from "../utils/config";
 
-// export const pool = new Pool({
-//   connectionString: config.connection_string,
-// });
+export const pool = new Pool({
+  connectionString: config.connection_string,
+});
+
+export const initDB = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor',
+    CHECK (role IN ('contributor', 'maintainer')),
+
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    );
+    `);
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS issues(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    CHECK (LENGTH(description) >= 20),
+
+    type VARCHAR(50) NOT NULL,
+    CHECK (type IN ('bug', 'feature_request')),
+
+    status VARCHAR(20) NOT NULL DEFAULT 'open',
+    CHECK (status IN ('open', 'in_progress', 'resolved')),
+
+    reporter_id INTEGER NOT NULL,
+
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
+import { initDB } from "./db";
 import config from "./utils/config";
 
 const main = async () => {
+  initDB();
   app.listen(config.port, () => {
     console.log(`ResolveHQ server running on port ${config.port}`);
   });
 };
 
-main()
+main();
